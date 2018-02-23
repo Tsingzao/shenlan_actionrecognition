edac9862a0a93c9fd7cd74af164cee2584fc6330
Tsingzao <yutingzhaomeng@hotmail.com>
2018-02-23 08:52:42 +0800

    Update two_stream.py

--- two_stream.py
+++ two_stream.py
@@ -5,6 +5,7 @@ Created by Tsingzao
 
 """
 
+'''=========================================================================='''
 import os
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 os.system('echo $CUDA_VISIBLE_DEVICES')
@@ -17,6 +18,12 @@ set_session(tf.Session(config=config))
 
 from keras.models import Model
 from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense, Dropout, Input, merge
+
+'''=========================================================================='''
+
+'''prepare your video data'''
+
+'''=========================================================================='''
 
 '''=========================================================================='''
 spatial_input = Input((224, 224, 3))
